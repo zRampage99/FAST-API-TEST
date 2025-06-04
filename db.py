f877351c87fd7518de6b1bcfe86e161885d6cea5
Lorenzo Gaetani <lorenzo.gaetani.6@gmail.com>
2025-06-04 10:26:45 +0200

    spostato il db da sqlite a mysql

--- db.py
+++ db.py
@@ -1,6 +1,17 @@
-from sqlmodel import create_engine, Session
+from sqlmodel import SQLModel, create_engine, Session
+import os
 
-engine = create_engine("sqlite:///database.db")
+# Costruisci l'URL del database
+MYSQL_USER = os.getenv("MYSQL_USER", "root")
+MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "root")
+MYSQL_HOST = os.getenv("MYSQL_HOST", "localhost")
+MYSQL_PORT = os.getenv("MYSQL_PORT", "3306")
+MYSQL_DB = os.getenv("MYSQL_DB", "python-test")
+
+DATABASE_URL = f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+
+# Crea l'engine
+engine = create_engine(DATABASE_URL, echo=True)
 
 def get_session():
     with Session(engine) as session:
--- entity/item.py
+++ entity/item.py
@@ -4,7 +4,6 @@ from sqlalchemy import String, Numeric
 from typing import Optional
 from sqlmodel import SQLModel
 
-
 class Item(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     name: str = Field(sa_column=String(50))
